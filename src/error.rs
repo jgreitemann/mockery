4a8e680962b899d2b18c889d14ed08dbb74b5357
Jonas Greitemann <jgreitemann@gmail.com>
2021-11-06 16:08:17 +0100

    Colorize and italicize error message

--- src/error.rs
+++ src/error.rs
@@ -22,31 +22,38 @@ impl Display for CLIError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             CLIError::SourceFileNotFound(io_err) => {
-                f.write_fmt(format_args!("Failed to open source file: {}", io_err))
+                f.write_fmt(format_args!(
+                    "Failed to open source file: {}",
+                    io_err.to_string().italic(),
+                ))
             }
             CLIError::SpecifiedCompilationDatabaseNotFound(io_err) => {
-                f.write_fmt(format_args!("Could not find the specified compile commands database: {}", io_err))
+                f.write_fmt(format_args!(
+                    "Could not find the specified compile commands database: {}",
+                    io_err.to_string().italic(),
+                ))
             }
             CLIError::CompilationDatabaseSearchFailed => {
                 f.write_str("Could not find compile commands database within the specified search radius")
             }
             CLIError::CompilationDatabaseSearchStartingPointNotFound(starting_point, io_err) => {
                 f.write_fmt(format_args!(
-                "Could not find the starting point '{}' for the compile commands database search: {}",
-                starting_point.to_str().unwrap(),
-                io_err))
+                    "Could not find the starting point '{}' for the compile commands database search: {}",
+                    starting_point.to_str().unwrap().yellow(),
+                    io_err.to_string().italic(),
+                ))
             }
             CLIError::CompileCommandNotFound(source_file) => f.write_fmt(format_args!(
                 "Failed to find compile command for '{}' in database",
-                source_file.to_str().unwrap()
+                source_file.to_str().unwrap().yellow(),
             )),
             CLIError::SourceError(source_err) => f.write_fmt(format_args!(
                 "The source file could not be parsed: {}",
-                source_err
+                source_err.to_string().italic(),
             )),
             CLIError::InterfaceClassNotFound(interface_name) => f.write_fmt(format_args!(
                 "No interface class named `{}` was found in the specified translation unit",
-                interface_name
+                interface_name.yellow(),
             )),
             CLIError::NotYetImplemented => f.write_str("Not yet implemented"),
         }
